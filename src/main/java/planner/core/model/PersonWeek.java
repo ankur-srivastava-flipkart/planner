49173056249f587139f1a09d166e5ac15f8296eb
Shikhar Sharad <shikhar.sharad@flipkart.com>
2017-06-23 05:49:34 +0530

    Adding leave construct

--- src/main/java/planner/core/model/PersonWeek.java
+++ src/main/java/planner/core/model/PersonWeek.java
@@ -1,10 +1,9 @@
 package planner.core.model;
 
-import java.time.LocalDate;
-
 /**
  * Created by ankur.srivastava on 23/06/17.
  */
+
 public class PersonWeek {
     TeamMember person;
     Week week;
