2f6ec2895cd54ebfb31a2cb5e39f3fab1c3748b9
Shikhar Sharad <shikhar.sharad@flipkart.com>
2017-06-23 08:31:56 +0530

    updating occupied during oncall swap

--- src/main/java/planner/core/model/PlannerMain.java
+++ src/main/java/planner/core/model/PlannerMain.java
@@ -8,6 +8,7 @@ public class PlannerMain {
     Planner planner = new Planner("JFM");
     //planner.addLeave(TeamMember.SHIKHAR, new LocalDate("2017-02-03"), new LocalDate("2017-02-10"));
     planner.addLeave(TeamMember.SHIKHAR, new LocalDate("2016-12-30"), new LocalDate("2017-01-05"));
+    planner.addLeave(TeamMember.AMIT, new LocalDate("2016-12-30"), new LocalDate("2017-01-05"));
     planner.printPlan();
   }
 }
