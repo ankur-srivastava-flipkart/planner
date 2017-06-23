a18cefc37a75eeab25414798f16ee3866fc3a87d
Shikhar Sharad <shikhar.sharad@flipkart.com>
2017-06-23 12:45:40 +0530

    added action resource

--- src/main/java/planner/core/model/PlannerResource.java
+++ src/main/java/planner/core/model/PlannerResource.java
@@ -1,7 +1,10 @@
 package planner.core.model;
 
+import com.google.common.collect.Maps;
+import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Provider;
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,6 +15,7 @@ import javax.ws.rs.Produces;
  * Created by kumar.vivek on 23/06/17.
  */
 @Path("/planner")
+@Singleton
 public class PlannerResource {
 
   private Planner planner;
@@ -32,6 +36,25 @@ public class PlannerResource {
   @Path("/{quarter}/plan/reset")
   public void resetQuarterPlan(@PathParam("quarter") String quarter){
     planner.reset(quarter);
+  }
+
+  @POST
+  @Path("/action")
+  public String takeAction(Action action){
+    switch (action.action) {
+      case "fetch_task": break;
+      case "init_qtr_plan": break;
+      case "modify_leave": break;
+      case "modify_oncall": break;
+    }
+    return "Done";
+  }
+
+
+  public static class Action {
+    String action;
+    String actor;
+    Map<String, String> param = Maps.newHashMap();
   }
 
 }
